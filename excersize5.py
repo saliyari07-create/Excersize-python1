fe2644442c8c6afaca290755e5deedadb415ba94
saliyari07-create <saliyari07@gmail.com>
2025-11-08 22:08:18 +0330

    Add files via upload

---
+++ excersize5.py
@@ -0,0 +1,20 @@
+while True:
+  z=int(input('enter number:\n'))
+  if z==1:
+    print('shanbe')
+  elif z==2:
+    print('1shanbe')
+  elif z==3:
+    print('2 shanbe')
+  elif z==4:
+    print('3 shanbe')
+  elif z==5:
+    print('4 shanbe')
+  elif z==6:
+    print('5 shanbe')
+  elif z==7:
+    print('jomeh')
+  else:
+    print('Error')
+         
+  

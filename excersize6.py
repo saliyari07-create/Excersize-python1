b4b448696ceb85e611d4b7536c11ec4d0dc1cd96
saliyari07-create <saliyari07@gmail.com>
2025-11-09 15:14:58 +0330

    Add files via upload

---
+++ excersize6.py
@@ -0,0 +1,18 @@
+while True:
+  w=input("Enter a day:\n")
+  if w=="shanbe":
+      print('1')
+  elif w=="yekshanbe":
+       print('2')
+  elif w=="doshanbe":
+       print('3')
+  elif w=="seshanbe":
+       print('4')
+  elif w=="chaharshanbe":
+        print('5')
+  elif w=="panjshanbe":
+        print('6')
+  elif w=="jomeh":
+        print('7')
+  else:
+           print("Error")
